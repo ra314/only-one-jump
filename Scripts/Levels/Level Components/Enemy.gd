b991b85508882a79e550d9afa345ab6f72f578a5
Rifle D. Luffy <theproplayer2@gmail.com>
2022-07-02 12:00:05 +0300

    Added an enemy, you can jump on an enemy, enemy resets if player resets

--- Scripts/Levels/Level Components/Enemy.gd
+++ Scripts/Levels/Level Components/Enemy.gd
@@ -1,18 +1,14 @@
 extends KinematicBody2D
 class_name Enemy
 
-const ACCELERATION_GROUND = 300
-const ACCELERATION_AIR = 75
-const JUMP_SPEED = 1200
-const FRICTION_GROUND = 30
-const FRICTION_AIR = 20
-const MAX_SPEED = 400
+const MAX_SPEED = 250
 
 const FALL_MULTIPLIER = 2.5;
 
 var velocity = Vector2(0, 0)
-var is_jumping = false
-var has_jumped = false
+var dir = Vector2(1, 0)
+var move_right = false
+var dead = false
 
 onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -20,47 +16,39 @@ var init_position
 var init_direction
 var init_velocity
 func _ready():
+	get_parent().get_node("Player1").connect("on_reset", self, "reset")
 	init_position = position
 	init_velocity = velocity
 	reset()
 
 func _process(delta):
-	if Input.is_action_pressed("move_right"):
-		if is_on_floor(): 
-			velocity += Vector2(ACCELERATION_GROUND, 0)
-		else:
-			velocity += Vector2(ACCELERATION_AIR, 0)
-	if Input.is_action_pressed("move_left"):
-		if is_on_floor(): 
-			velocity -= Vector2(ACCELERATION_GROUND, 0)
-		else:
-			velocity -= Vector2(ACCELERATION_AIR, 0)
+	if dead:
+		return
+		
+	if is_on_wall():
+		print("im on the wall")
+		move_right = not move_right
+		
+		var sprite := $Sprite as Sprite
+		sprite.scale *= Vector2(-1, 1)
+		print(velocity)
 	
-	# We use just_pressed so that the player can't hold jump infinitely to fly
-	if Input.is_action_just_pressed("jump") and is_on_floor() and not has_jumped:
-		velocity.y = -JUMP_SPEED
-		has_jumped = true
-		is_jumping = true
-	if Input.is_key_pressed(KEY_X):
-		reset()
-	# Die below a certain height
-	if position.y > 2000:
-		reset()
-	# Applying max speed
+	if move_right:
+		velocity.x = MAX_SPEED
+	else:
+		velocity.x = -MAX_SPEED
+
 	velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
 
 func _physics_process(delta):
+	if dead:
+		return
+		
 	# Vertical movement code. Apply gravity.	
-	if !is_on_floor():
-		# FRICTION_AIR when on the floor
-		velocity.x = sign(velocity.x) * max(0, abs(velocity.x) - FRICTION_AIR)
 			
-	if is_on_floor() and !is_jumping:
+	if is_on_floor():
 		velocity.y = 0
-		# FRICTION_GROUND when on the floor
-		velocity.x = sign(velocity.x) * max(0, abs(velocity.x) - FRICTION_GROUND)
 	elif velocity.y < 0:
-		is_jumping = false
 		velocity.y += gravity * (FALL_MULTIPLIER - 1) * delta
 	else:
 		velocity.y += gravity * delta
@@ -71,10 +59,30 @@ func _physics_process(delta):
 func reset():
 	position = init_position
 	velocity = init_velocity
-	has_jumped = false
+	
+	dead = false
+	$"Normal Hitbox".disabled = false
+	$"Collision Check/Jump Check".disabled = false
+	visible = true
+	
+func on_die():
+	dead = true
+	
+	$"Normal Hitbox".disabled = true
+	$"Collision Check/Jump Check".disabled = true
+	visible = false
 
-func action():
-	# Check for jumping. is_on_floor() must be called after movement code.
-	if is_on_floor():
-		velocity.y = -JUMP_SPEED
-		is_jumping = true
+func _on_Area2D_body_entered(body):
+	if dead:
+		return
+	
+	if body.name != "Player1":
+		return
+	
+	var player = body as Player
+	
+	var is_player_above = body.position.y < self.position.y
+	if is_player_above and !player.is_on_floor():
+		player.jump(true)
+		
+		on_die()
--- Scripts/Levels/Level Components/player.gd
+++ Scripts/Levels/Level Components/player.gd
@@ -1,12 +1,14 @@
 extends KinematicBody2D
 class_name Player
 
+# This is roughly 3.25 blocks high
+const JUMP_SPEED = 1200
+const MAX_SPEED = 400
+
 const ACCELERATION_GROUND = 300
 const ACCELERATION_AIR = 75
-const JUMP_SPEED = 1200
 const FRICTION_GROUND = 30
 const FRICTION_AIR = 20
-const MAX_SPEED = 400
 
 const FALL_MULTIPLIER = 2.5;
 
@@ -37,7 +39,7 @@ func _process(delta):
 			velocity -= Vector2(ACCELERATION_AIR, 0)
 	
 	# We use just_pressed so that the player can't hold jump infinitely to fly
-	if Input.is_action_just_pressed("jump") and is_on_floor() and not has_jumped:
+	if Input.is_action_just_pressed("jump") and not has_jumped:
 		velocity.y = -JUMP_SPEED
 		has_jumped = true
 		is_jumping = true
@@ -48,6 +50,16 @@ func _process(delta):
 		reset()
 	# Applying max speed
 	velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
+
+func jump(force_jump = false) -> void:
+	if (!force_jump and has_jumped):
+		return
+
+	if (!force_jump):
+		has_jumped = true
+
+	velocity.y = -JUMP_SPEED
+	is_jumping = true
 
 func _physics_process(delta):
 	# Vertical movement code. Apply gravity.	
@@ -68,10 +80,13 @@ func _physics_process(delta):
 	# Move based on the velocity and snap to the ground.
 	move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
 
+signal on_reset
 func reset():
 	position = init_position
 	velocity = init_velocity
 	has_jumped = false
+	
+	emit_signal("on_reset")
 
 func action():
 	# Check for jumping. is_on_floor() must be called after movement code.
