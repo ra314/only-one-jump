d88d002cbfa56f4c1dd764ab8b34aab5d89c681c
Rifle D. Luffy <theproplayer2@gmail.com>
2022-07-03 10:35:05 +0300

    Tweak stalagmite and button code. Skinned them

--- Scenes/Levels/Level Components/Pressure Plate.gd
+++ Scenes/Levels/Level Components/Pressure Plate.gd
@@ -3,6 +3,7 @@ class_name Pressure_Plate
 
 func _ready():
 	connect("body_entered", self, "_on_body_entered")
+	connect("activated", self, "switch_state")
 
 signal activated
 func _on_body_entered(body) -> void:
@@ -12,3 +13,7 @@ func _on_body_entered(body) -> void:
 		var is_player_above = body.position.y < self.position.y
 		if is_player_above:
 			emit_signal("activated")
+			
+func switch_state() -> void:
+	$On.visible = false
+	$Off.visible = true
--- Scenes/Levels/Level Components/Stalagmite.gd
+++ Scenes/Levels/Level Components/Stalagmite.gd
@@ -2,23 +2,41 @@ extends KinematicBody2D
 class_name Stalagmite
 
 const FALL_MULTIPLIER = 2.5;
+const FRICTION_GROUND = 30
+const FRICTION_AIR = 20
+
 onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var falling: bool = false
-var velocity: Vector2 = Vector2(0,0)
+export (Vector2) var velocity = Vector2(0,0)
+export (bool) var triggered = false
+export (bool) var on_floor = false
 
 func _ready():
 	pass
 
 func _physics_process(delta):
-	var space_state = get_world_2d().direct_space_state
-	# use global coordinates, not local to node
-	var result = space_state.intersect_ray(global_position, global_position + Vector2(0,500), [self])
-	if len(result.keys()) != 0:
-		if result["collider"].name == Utils.PLAYER_NAME:
-			falling = true
-	
-	if falling:
+	check_object_below()	
+			
+	on_floor = is_on_floor()
+	if is_on_floor():
+		falling = false
+		velocity.x = sign(velocity.x) * max(0, abs(velocity.x) - FRICTION_GROUND)
+	elif triggered:
+		falling = true
+		velocity.x = sign(velocity.x) * max(0, abs(velocity.x) - FRICTION_AIR)
 		velocity.y += gravity * delta
 		
 	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP,
 					false, 4, PI/4, false)
+
+func check_object_below() -> void:
+	var space_state = get_world_2d().direct_space_state
+	# use global coordinates, not local to node
+	var result = space_state.intersect_ray(global_position, global_position + Vector2(0,500), [self])
+	if len(result.keys()) != 0:
+		if result["collider"] == null:
+			return
+			
+		var collider = result["collider"] as PhysicsBody2D
+		if collider != null and collider.is_in_group("bodies"):
+			triggered = true
